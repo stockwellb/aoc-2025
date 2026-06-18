85b29ee40ae73d98418747ccaacce4407e9dd380
Bruce Stockwell <bruce.stockwell@gmail.com>
2026-06-18 15:33:16 -0400

    Day one SAT

--- day01/main.c
+++ day01/main.c
@@ -1,16 +1,33 @@
 #include <stdio.h>
-#include "../lib/aoc.h"
 
 int main(int argc, char **argv) {
-	const char *filename = argc > 1 ? argv[1] : "input.txt";
-	printf("Advent of Code 2025 - Day 01\n");
-	printf("Using input: %s\n", filename);
+  const char *filename = argc > 1 ? argv[1] : "input.txt";
+  printf("Advent of Code 2025 - Day 01\n");
+  printf("Using input: %s\n", filename);
 
-	// StringArray lines = read_lines(filename);
-	// for (size_t i = 0; i < lines.count; i++) {
-	//     printf("%s\n", lines.items[i]);
-	// }
-	// free_lines(&lines);
+  if (!freopen(filename, "r", stdin)) {
+    perror(filename);
+    return 1;
+  }
 
-	return 0;
+  char dir;
+  int dist;
+  int pos = 50;
+  int count = 0;
+
+  while (scanf(" %c%d", &dir, &dist) == 2) {
+    if (dir == 'L') {
+      dist = -dist;
+    }
+    pos = (pos + dist) % 100;
+    if (pos < 0)
+      pos += 100;
+    if (pos == 0) {
+      count++;
+    }
+  }
+
+  printf("password = %d\n", count);
+
+  return 0;
 }
