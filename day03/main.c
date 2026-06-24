7d0a7087e93abf70a20c57c1ad25f5e5890bf7b4
Bruce Stockwell <bruce.stockwell@gmail.com>
2026-06-24 09:05:26 -0400

    day03 wip

--- day03/main.c
+++ day03/main.c
@@ -10,5 +10,19 @@ int main(int argc, char **argv) {
   printf("Advent of Code 2025 - Day 03\n");
   printf("Using input: %s\n", filename);
 
+  FILE *fp = fopen(filename, "r");
+
+  if (!fp) {
+    return 1;
+  }
+
+  char line[100];
+
+  while (fgets(line, sizeof(line), fp)) {
+    for (char *p = line; *p && *p != '\n'; p++) {
+      printf("%c\n", *p);
+    }
+  }
+
   return 0;
 }
